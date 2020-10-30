41d2badf4f83686f841aa35a3f69695820165748
Masaki Iwahara <3982522+iwahara@users.noreply.github.com>
2020-10-30 22:58:32 +0900

    install koin

--- src/Application.kt
+++ src/Application.kt
@@ -5,12 +5,16 @@ import io.ktor.response.*
 import io.ktor.request.*
 import io.ktor.routing.*
 import io.ktor.http.*
+import org.koin.ktor.ext.Koin
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    install(Koin) {
+
+    }
     routing {
         get("/") {
             call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
